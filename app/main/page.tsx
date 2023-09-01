97da1bc65f34254c27a01a8f0a59bb94e9d23bb9
HaekyungSeong <haekyungs95@gmail.com>
2023-09-01 13:51:13 +0900

    a 태그 Link 컴포넌트로 전환 및 캡슐 클릭 시 링크 연동, CSS 클래스 변경

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,16 +1,45 @@
+"use client";
+import { useState } from "react";
 import styles from "../../styles/page.module.css";
+import Link from "next/link";
 
 export default function Page() {
+  const [open, Setopen] = useState(false);
+
   return (
     <>
-      <div className={styles.topBox}>
-        <div className={styles.whiteSmallCap}></div>
-        <div className={styles.blueSmallCap}></div>
+      {/* 상단에 작은 캡슐모양 */}
+      <div className={`${styles.topBox} ${styles.flexRowCenter}`}>
+        <div
+          className={styles.whiteSmallCap}
+          onClick={() => {
+            history.go(-1);
+          }}
+        ></div>
+        <Link className={styles.blueSmallCap} href="/"></Link>
       </div>
-      <div className={styles.bottom}>
-        <div>
-          <a href="http://127.0.0.1:3000/info">약품 신상</a>
-          <a href="http://127.0.0.1:3000/project">투약 프로젝트</a>
+
+      <div className={`${styles.bottom} ${styles.flexRowCenter}`}>
+        <div
+          onClick={() => {
+            if (open) {
+              Setopen(false);
+            } else {
+              Setopen(true);
+            }
+          }}
+        >
+          약품정보 상세보기 ▼
+          {open && (
+            <div className={`${styles.toggle} ${styles.flexColumnCenter}`}>
+              <Link className={styles.link} href="/info">
+                약품 신상
+              </Link>
+              <Link className={styles.link} href="/project">
+                투약 프로젝트
+              </Link>
+            </div>
+          )}
         </div>
       </div>
     </>
