92035b47c3017d1a598bdf5510113b20e5eee729
HaekyungSeong <haekyungs95@gmail.com>
2023-09-01 13:00:48 +0900

    구글 폰트 적용

--- app/main/layout.tsx
+++ app/main/layout.tsx
@@ -1,9 +1,10 @@
 import "../../styles/global.css";
+import { gamja } from "../../styles/font.ts";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={gamja.className}>{children}</body>
     </html>
   );
 }
---
+++ styles/font.ts
@@ -0,0 +1,8 @@
+import { Gamja_Flower } from "next/font/google";
+
+export const gamja = Gamja_Flower({
+  weight: "400",
+  style: "normal",
+  display: "swap",
+  subsets: ["latin"],
+});
