c64efe9b5e21abd6ca10ed0fbd6fb7fb500c1cb8
HaekyungSeong <haekyungs95@gmail.com>
2023-09-01 13:50:40 +0900

    a 태그 Link 컴포넌트로 변경 및 CSS 클래스 들 적용

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -1,13 +1,14 @@
+import Link from "next/link";
 import styles from "../../styles/page.module.css";
 
 export default function Page() {
   return (
     <>
-      <a className={styles.topBox} href="http://127.0.0.1:3000/main">
+      <Link className={`${styles.topBox} ${styles.flexRowCenter}`} href="/main">
         <div className={styles.whiteSmallCap}></div>
         <div className={styles.blueSmallCap}></div>
-      </a>
-      <div className={styles.bottom}>내 정보</div>
+      </Link>
+      <div className={`${styles.bottom} ${styles.flexRowCenter}`}>내 정보</div>
     </>
   );
 }
