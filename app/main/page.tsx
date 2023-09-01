dd0ec2ad43a483c8b9115c41cbab6c91851e7971
HaekyungSeong <haekyungs95@gmail.com>
2023-09-01 15:26:00 +0900

    main 페이지 구성 완료.-> 세분화한 css 조정은 추후 진행하기.

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -2,6 +2,8 @@
 import { useState } from "react";
 import styles from "../../styles/page.module.css";
 import Link from "next/link";
+import Image from "next/image";
+import photo from "../../Utils/230910.jpg";
 
 export default function Page() {
   const [open, Setopen] = useState(false);
@@ -15,12 +17,29 @@ export default function Page() {
           onClick={() => {
             history.go(-1);
           }}
-        ></div>
-        <Link className={styles.blueSmallCap} href="/"></Link>
+        />
+        <Link className={styles.blueSmallCap} href="/" />
       </div>
 
-      <div className={`${styles.bottom} ${styles.flexRowCenter}`}>
+      {/* 본문내용 부분 */}
+      <div className={`${styles.bottomMain} ${styles.flexColumnCenter}`}>
+        <h2>생기 넘치는 비타민, "성해경"</h2>
+        <h3>복용 한 번 해보실래요?💊</h3>
+        <div className={`${styles.flexRowCenter} ${styles.mainsub}`}>
+          <Image
+            src={photo}
+            width={170}
+            height={200}
+            alt="zoe's picture"
+            draggable={false}
+            className={styles.image}
+          />
+          <div className={`${styles.title}`}>
+            단, 단시간 복용하였음에도 <br /> 장기간 복용한 듯한 부작용이 있을 수 있습니다.😉
+          </div>
+        </div>
         <div
+          className={styles.toggleButton}
           onClick={() => {
             if (open) {
               Setopen(false);
@@ -31,12 +50,12 @@ export default function Page() {
         >
           약품정보 상세보기 ▼
           {open && (
-            <div className={`${styles.toggle} ${styles.flexColumnCenter}`}>
+            <div className={`${styles.toggle} ${styles.flexRowCenter}`}>
               <Link className={styles.link} href="/info">
-                약품 신상
+                📒약품 신상📒
               </Link>
               <Link className={styles.link} href="/project">
-                투약 프로젝트
+                💊투약 프로젝트💊
               </Link>
             </div>
           )}
