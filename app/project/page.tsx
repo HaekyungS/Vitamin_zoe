e711d9bac043499966f7c96614f10dc60b76e2e7
HaekyungSeong <haekyungs95@gmail.com>
2023-09-04 16:53:36 +0900

    탭 클릭 시 해당 컴포넌트로 반환하도록 로직 구성

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,7 +1,28 @@
+"use client";
+
 import Link from "next/link";
 import styles from "../../styles/page.module.css";
+// import Image from "next/image";
+// import pokeXdigi from "../../Utils/pokeXdigi.png";
+// import pokeXdigi_env from "../../Utils/pokeXdigi_dev_env.png";
+import { PokeXDigi } from "./component/PokeXDigi";
+import { useEffect, useState } from "react";
 
 export default function Page() {
+  const [project, setProject] = useState("PokeXDigi");
+
+  const projectName = () => {
+    if (project === "PokeXDigi") {
+      return <PokeXDigi />;
+    } else if (project === "JamStock") {
+      return <div>JamStock</div>;
+    } else if (project === "Mucbo") {
+      return <div>오리오랜만</div>;
+    } else {
+      return <div>뭔데 이건</div>;
+    }
+  };
+
   return (
     <>
       <div className={`${styles.topBox} ${styles.flexRowCenter}`}>
@@ -13,7 +34,32 @@ export default function Page() {
         ></div>
         <Link className={styles.blueSmallCap} href="/"></Link>
       </div>
-      <div className={`${styles.bottom} ${styles.flexRowCenter}`}>프로젝트</div>
+      <div className={`${styles.bottom} ${styles.flexColumnCenter}`}>
+        <div className={`${styles.flexRowSAround} ${styles.projectTap}`}>
+          <div
+            onClick={() => {
+              setProject("PokeXDigi");
+            }}
+          >
+            POKE X DIGI
+          </div>
+          <div
+            onClick={() => {
+              setProject("JamStock");
+            }}
+          >
+            JamStock
+          </div>
+          <div
+            onClick={() => {
+              setProject("Mucbo");
+            }}
+          >
+            먹보의 하루
+          </div>
+        </div>
+        {projectName()}
+      </div>
     </>
   );
 }
