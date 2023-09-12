023ca07d7ce2b45a576d9dd50e6d9c4ba0deca40
HaekyungSeong <haekyungs95@gmail.com>
2023-09-12 09:43:07 +0900

    불필요한 중복파일 제거 및 layout 폰트변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import "../styles/global.css";
+import { poorS } from "../styles/font";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="ko" className={poorS.className}>
       <body>{children}</body>
     </html>
   );
