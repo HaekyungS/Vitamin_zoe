915d6170d7c00f858a8c300379af61f9012df4e2
HaekyungSeong <haekyungs95@gmail.com>
2023-09-12 09:44:25 +0900

    폰트 종류 추가

--- styles/font.ts
+++ styles/font.ts
@@ -1,6 +1,14 @@
 import { Gamja_Flower } from "next/font/google";
+import { Poor_Story } from "next/font/google";
 
 export const gamja = Gamja_Flower({
+  weight: "400",
+  style: "normal",
+  display: "swap",
+  subsets: ["latin"],
+});
+
+export const poorS = Poor_Story({
   weight: "400",
   style: "normal",
   display: "swap",
