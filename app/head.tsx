c7336fcc89667d1b25575d858f5f25c2d6a0394e
HaekyungSeong <haekyungs95@gmail.com>
2023-09-15 15:49:01 +0900

    head 추가작성

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+export const Head = () => {
+  return (
+    <>
+      <title>개발자 성해경입니다🤗</title>
+      <meta
+        name="description"
+        content="비타민 같은 개발자, 성해경입니다! 신나게 개발하지 않으실래요?"
+      />
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+    </>
+  );
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import "../styles/global.css";
 import { poorS } from "../styles/font";
+import { Head } from "./head";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko" className={poorS.className}>
+      <Head />
       <body>
         {children}
         <footer>CopyrightⓒPowered by 성해경 All Right Reserved</footer>
